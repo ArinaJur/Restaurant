59a4e9f144b571fa17d2ad50a2e46d29b8eec33f
ArinaJur <66705803+ArinaJur@users.noreply.github.com>
2021-04-21 00:56:46 -0400

    created fiels and constructor in BaseDish, MainDish extends BaseDish (#1)
    Co-authored-by: AniriRuj <Rjntyjr12#>

--- src/BaseDish.java
+++ src/BaseDish.java
@@ -1,2 +1,17 @@
-public class BaseDish {
+public abstract class BaseDish {
+    private String name;
+    private double price;
+
+    public BaseDish(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
--- src/MainDish.java
+++ src/MainDish.java
@@ -1,25 +1,6 @@
-public class MainDish {
-    private String mainDishName;
-    private double mainDishPrice;
+public class MainDish extends BaseDish {
 
     MainDish(String name, double price) {
-        this.mainDishName = name;
-        this.mainDishPrice = price;
-    }
-
-    public String getMainDishName() {
-        return mainDishName;
-    }
-
-    public double getMainDishPrice() {
-        return mainDishPrice;
-    }
-
-    public void setMainDishName(String name) {
-        this.mainDishName = name;
-    }
-
-    public void setMainDishPrice(double price) {
-        this.mainDishPrice = price;
+        super(name, price);
     }
 }
